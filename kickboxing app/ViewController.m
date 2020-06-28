ccd0171bd35ee4af292f8f56092a183ca7e608cc
Bader Kharbat <Unconcernedproductions@gmail.com>
2020-06-28 13:03:03 -0400

    WorkingUI Functionality

--- kickboxing app/ViewController.m
+++ kickboxing app/ViewController.m
@@ -8,9 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDataSource, UITableViewDelegate>
 @property (strong, nonatomic) IBOutlet UIStepper *stepper;
 @property (strong, nonatomic) IBOutlet UILabel *countLabel;
+@property (strong, nonatomic) IBOutlet UITableView *table;
+@property NSMutableArray * moveArray;
 @property MoveManager * manager;
 @end
 
@@ -30,6 +32,7 @@
     self.stepper.maximumValue = 10;
     self.stepper.value = 4;
     self.countLabel.text = [NSString stringWithFormat:@"%1.0f", self.stepper.value];
+//    self.moveArray = [NSMutableArray arrayWithArray:@[self.manager.moveList[0]]];
 }
 
 - (IBAction)onStepperChange:(UIStepper *)sender {
@@ -37,12 +40,27 @@
 }
 
 - (IBAction)generateListOfMoves:(UIButton *)sender {
-    NSMutableArray * rv = [self.manager generate:self.stepper.value];
-    for(int i = 0; i < rv.count; i++) {
-        NSLog(@"%@", [(Move *) rv[i] name]);
+    self.moveArray = [self.manager generate:self.stepper.value];
+    [self.table reloadData];
+    
+    for(int i = 0; i < self.moveArray.count; i++) {
+        NSLog(@"%@", [(Move *) self.moveArray[i] name]);
     }
     NSLog(@"------");
 }
 
+#pragma mark - UITableView DataSource Methods
+- (NSInteger)tableView:(nonnull UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return _moveArray.count;
+}
+
+- (nonnull UITableViewCell *)tableView:(nonnull UITableView *)tableView cellForRowAtIndexPath:(nonnull NSIndexPath *)indexPath {
+    static NSString * cellId = @"cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellId];
+    cell.textLabel.text = [(Move *) self.moveArray[indexPath.row] name];
+    cell.detailTextLabel.text = @"";
+    
+    return cell;
+}
 
 @end

7900e65ca6340281bf491958e2befbc944c7401b
Bader Kharbat <Unconcernedproductions@gmail.com>
2020-07-19 23:05:25 -0400

    Added Logo and modified UI

--- kickboxing app/ViewController.m
+++ kickboxing app/ViewController.m
@@ -45,7 +45,8 @@
     self.bannerView = [[GADBannerView alloc]
         initWithAdSize:kGADAdSizeBanner];
     [self addBannerViewToView:self.bannerView];
-    self.bannerView.adUnitID = @"ca-app-pub-3940256099942544/2934735716";
+//    self.bannerView.adUnitID = @"ca-app-pub-3940256099942544/2934735716"; //test
+    self.bannerView.adUnitID = @"ca-app-pub-8286027185402342/9907216040";
     self.bannerView.rootViewController = self;
     [self.bannerView loadRequest:[GADRequest request]];
 }
